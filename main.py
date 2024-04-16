f39da02477bf313422cce6ebd33a9eb43d363001
sahil-chhoker <chhokersahil435@gmail.com>
2024-04-17 00:32:16 +0530

    text to pdf complete

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from pypdf import PdfReader
 from pathlib import Path
+from fpdf import FPDF
 
-pdf_path = (Path.home()/ 'C:\MASTER FOLDER\GitHub\PDF-Editor\sample.pdf')
+def convert_file(file):
+    pdf = FPDF()
+    pdf.add_page()
 
-pdf_reader = PdfReader(pdf_path)
+    for text in file:
+        if len(text) <= 20:
+            pdf.set_font("Arial", "B", size=18)
+            pdf.cell(w=200, h=10, txt=text, ln=1, align="C")
+        else:
+            pdf.set_font("Arial", size=15)
+            pdf.multi_cell(w=0, h=10, txt=text,align="L")
+    pdf.output("result.pdf")
 
-for page in pdf_reader.pages:
-    print(page.extract_text())
+if __name__ == "__main__":
+    text_file = Path("C:/MASTER FOLDER/GitHub/PDF-Editor/test/BIODIVERSITY.txt")
+    with open(text_file, 'r') as file:
+        convert_file(file)
