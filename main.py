21f3c87a2ee82da08319476dde628feded52d919
sahil-chhoker <chhokersahil435@gmail.com>
2024-04-18 18:51:12 +0530

    made a gui for splitting the text as well

--- main.py
+++ main.py
@@ -7,31 +7,25 @@ from PyPDF2 import PdfReader, PdfWriter
 def get_file_name(file_path):
     return Path(file_path).stem
 
-# ? : Converting text files to pdf 
 def convert_file(file_path):
     pdf = FPDF()
     pdf.add_page()
 
-    fname = ''
+    fname = get_file_name(file_path)
     with open(file_path, 'r') as file:
-        fname = get_file_name(file.name)
         for text in file:
             pdf.set_font("Arial", size=10)
             pdf.multi_cell(w=0, h=10, txt=text, align="L")
         
     pdf.output(f"{fname}.pdf")
 
-# ? : Splitting pdf from start to page_no
 def splitting_pdf(file_path, page_no):
     fname = get_file_name(file_path)
     pdf = PdfReader(file_path)
     
     pdf_writer = PdfWriter()
-    if page_no == 1:
-        pdf_writer.add_page(pdf.pages[page_no - 1])
-    else:
-        for i in range(1, page_no + 1):
-            pdf_writer.add_page(pdf.pages[i - 1])
+    for i in range(page_no):
+        pdf_writer.add_page(pdf.pages[i])
         
     output_filename = f"{fname}_page_{page_no}.pdf"
 
@@ -40,19 +34,42 @@ def splitting_pdf(file_path, page_no):
 
     print('Created: {}'.format(output_filename))
 
-
-def select_file():
-    file_path = filedialog.askopenfilename(title="Select Text File", filetypes=[("Text files, Pdf files", "*.txt, *.pdf")])
+def select_file_and_action(action):
+    file_path = filedialog.askopenfilename(title="Select File", filetypes=[("Text files, Pdf files", "*.txt *.pdf")])
     if file_path:
-        splitting_pdf(file_path, 3)
+        if action == "convert":
+            convert_file(file_path)
+        elif action == "split":
+            page_no = int(entry.get())
+            splitting_pdf(file_path, page_no)
 
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x_coordinate = (screen_width / 2) - (width / 2)
+    y_coordinate = (screen_height / 2) - (height / 2)
+
+    window.geometry(f"{width}x{height}+{int(x_coordinate)}+{int(y_coordinate)}")
 
 def main():
     root = tk.Tk()
-    root.title("Text to PDF Converter")
+    root.title("File Converter")
+    root.resizable(False, False)  # Disable resizing
 
-    button = tk.Button(root, text="Select Text File", command=select_file)
-    button.pack(pady=20)
+    center_window(root, 300, 300)
+
+    convert_button = tk.Button(root, text="Convert Text to PDF", command=lambda: select_file_and_action("convert"))
+    convert_button.pack(pady=10)
+
+    split_button = tk.Button(root, text="Split PDF", command=lambda: select_file_and_action("split"))
+    split_button.pack(pady=10)
+
+    global entry
+    entry_label = tk.Label(root, text="Enter number of pages:")
+    entry_label.pack()
+    entry = tk.Entry(root)
+    entry.pack()
 
     root.mainloop()
 
