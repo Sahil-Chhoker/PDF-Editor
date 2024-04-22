6b8e1856aa016387de14ad81e5a954b9cb7abd1e
sahil-chhoker <chhokersahil435@gmail.com>
2024-04-22 15:25:46 +0530

    added a text size selector

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from fpdf import FPDF
 from PyPDF2 import PdfReader, PdfWriter, PdfMerger
 from pdf2image import convert_from_path
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 import groupdocs_conversion_cloud
 
 load_dotenv()
@@ -18,144 +18,148 @@ client_secret = os.getenv('CLIENT_SECRET')
 poppler_path = os.getenv('POPPLER_PATH')
 
 font_family = ["Courier", "Arial", "Times", "Symbol", "ZapfDingbats"]
+text_size = [2 * i for i in range(50)]
 
 
 def get_file_name(file_path):
-    return Path(file_path).stem
+   return Path(file_path).stem
 
 def random_file_no():
-    return random.randint(0, 1000)
+   return random.randint(0, 1000)
 
 
-def convert_txtfile_to_pdf(file_path, output_path, selected_font):
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font(selected_font, size=10)
-    
-    fname = get_file_name(file_path)
-    with open(file_path, 'r') as file:
-        for text in file:
-            pdf.multi_cell(w=0, h=10, txt=text, align="L")
-        
-    pdf.output(f"{output_path}/{fname}.pdf")
+def convert_txtfile_to_pdf(file_path, output_path, selected_font, selected_size):
+   pdf = FPDF()
+   pdf.add_page()
+   pdf.set_font(selected_font, size=selected_size)
+
+   fname = get_file_name(file_path)
+   with open(file_path, 'r') as file:
+       for text in file:
+           pdf.multi_cell(w=0, h=10, txt=text, align="L")
+
+   pdf.output(f"{output_path}/{fname}.pdf")
+
+   print(f"Files saved to: {output_path}")
 
 
 def splitting_pdf(file_path, page_no, trim_from_behind):
-    fname = get_file_name(file_path)
-    pdf = PdfReader(file_path)
-    no_of_pages = len(pdf.pages)
-    
-    pdf_writer = PdfWriter()
-    if not trim_from_behind:
-        for i in range(page_no):
-            pdf_writer.add_page(pdf.pages[i])
-    else: 
-        for i in range(page_no):
-            pdf_writer.add_page(pdf.pages[no_of_pages - page_no + i])
-        
-    output_filename = f"{fname}_page_{page_no}.pdf"
+   fname = get_file_name(file_path)
+   pdf = PdfReader(file_path)
+   no_of_pages = len(pdf.pages)
 
-    with open(output_filename, 'wb') as out:
-        pdf_writer.write(out)
+   pdf_writer = PdfWriter()
+   if not trim_from_behind:
+       for i in range(page_no):
+           pdf_writer.add_page(pdf.pages[i])
+   else:
+       for i in range(page_no):
+           pdf_writer.add_page(pdf.pages[no_of_pages - page_no + i])
 
-    print(f'Created: {output_filename}')
+   output_filename = f"{fname}_page_{page_no}.pdf"
+
+   with open(output_filename, 'wb') as out:
+       pdf_writer.write(out)
+
+   print(f'Created: {output_filename}')
 
 
 def merging_pdf(file_paths, output_path):
-    pdf_merger = PdfMerger()
-    fname = random_file_no()
+   pdf_merger = PdfMerger()
+   fname = random_file_no()
 
-    for file_path in file_paths:
-        with open(file_path, 'rb') as pdf:
-            pdf_merger.append(pdf)
+   for file_path in file_paths:
+       with open(file_path, 'rb') as pdf:
+           pdf_merger.append(pdf)
 
-    output_filename = f"{output_path}/merged_file{fname}.pdf"
+   output_filename = f"{output_path}/merged_file{fname}.pdf"
 
-    with open(output_filename, 'wb') as out:
-        pdf_merger.write(out)
+   with open(output_filename, 'wb') as out:
+       pdf_merger.write(out)
 
-    print(f'Merged: {output_filename}')
+   print(f'Merged: {output_filename}')
 
 
 def convert_to_image(file_path, output_path):
-    fname = get_file_name(file_path)
-    images = convert_from_path(file_path, poppler_path=poppler_path)
-    for i, image in enumerate(images):
-        image.save(f'{output_path}/{fname}_page_{i + 1}.jpg', 'JPEG')
+   fname = get_file_name(file_path)
+   images = convert_from_path(file_path, poppler_path=poppler_path)
+   for i, image in enumerate(images):
+       image.save(f'{output_path}/{fname}_page_{i + 1}.jpg', 'JPEG')
 
-    print(f'Images saved to {output_path}')
+   print(f'Images saved to {output_path}')
 
 
 def convert_to_ppt(file_path, output_path):
-    api = groupdocs_conversion_cloud.ConvertApi.from_keys(client_id, client_secret)
+   api = groupdocs_conversion_cloud.ConvertApi.from_keys(client_id, client_secret)
 
-    request = groupdocs_conversion_cloud.ConvertDocumentDirectRequest("pptx", file_path)
+   request = groupdocs_conversion_cloud.ConvertDocumentDirectRequest("pptx", file_path)
 
-    response = api.convert_document_direct(request)
-    shutil.move(response, output_path)
-    print(f'Converted PowerPoint saved to {output_path}')
+   response = api.convert_document_direct(request)
+   shutil.move(response, output_path)
+   print(f'Converted PowerPoint saved to {output_path}')
 
 
 def convert_img_files_to_pdf(file_paths, output_path):
-    image_data = []
-    fname = random_file_no()
+   image_data = []
+   fname = random_file_no()
 
-    for file_path in file_paths:
-        with open(file_path, 'rb') as img_file:
-            image_data.append(img_file.read())
-    
-    pdf_data = img2pdf.convert(image_data)
+   for file_path in file_paths:
+       with open(file_path, 'rb') as img_file:
+           image_data.append(img_file.read())
 
-    output_file = f'{output_path}/img_to_pdf{fname}.pdf'
-    with open(output_file, "wb") as file:
-        file.write(pdf_data)
+   pdf_data = img2pdf.convert(image_data)
 
-    print(f'Images converted and saved as PDF to: {output_path}')
+   output_file = f'{output_path}/img_to_pdf{fname}.pdf'
+   with open(output_file, "wb") as file:
+       file.write(pdf_data)
+
+   print(f'Images converted and saved as PDF to: {output_path}')
 
 
 def select_file_and_action(action):
-    file_path = filedialog.askopenfilename(title="Select File", filetypes=[("Text files, Pdf files", "*.txt *.pdf")])
-    if file_path:
-        if action == "split":
-            page_no = int(entry.get())
-            trim_from_behind = trim_var.get()
-            splitting_pdf(file_path, page_no, trim_from_behind)
-        elif action == "convert_to_image":
-            output_path = filedialog.askdirectory(title="Select Output Directory")
-            convert_to_image(file_path, output_path)
-        elif action == "convert_to_ppt":
-            output_path = filedialog.askdirectory(title="Select Output Directory")
-            convert_to_ppt(file_path, output_path)
+   file_path = filedialog.askopenfilename(title="Select File", filetypes=[("Text files, Pdf files", "*.txt *.pdf")])
+   if file_path:
+       if action == "split":
+           page_no = int(entry.get())
+           trim_from_behind = trim_var.get()
+           splitting_pdf(file_path, page_no, trim_from_behind)
+       elif action == "convert_to_image":
+           output_path = filedialog.askdirectory(title="Select Output Directory")
+           convert_to_image(file_path, output_path)
+       elif action == "convert_to_ppt":
+           output_path = filedialog.askdirectory(title="Select Output Directory")
+           convert_to_ppt(file_path, output_path)
 
 
 def select_multiple_files(action):
-    file_paths = filedialog.askopenfilenames(title="Select Files", filetypes=[("Text files, Pdf files, Image files", "*.txt *.pdf *.jpg *.jpeg *.png")])
-    if file_paths == None or file_paths == []:
-        raise ImportError("Files were not imported, try again!")
-    
-    if action == "convert":
-        for file_path in file_paths:
-            selected_font = font_combobox.get()
-            output_path = filedialog.askdirectory(title="Select Output Directory")
-            convert_txtfile_to_pdf(file_path, output_path, selected_font)
-    elif action == "merge":
-        if len(file_paths) <= 1:
-            raise ValueError("Can't merge a single file, select more than one to continue!")
-        output_path = filedialog.askdirectory(title="Select Output Directory")
-        merging_pdf(file_paths, output_path)
-    elif action == "convert_img_to_pdf":
-        output_path = filedialog.askdirectory(title="Select Output Directory")
-        convert_img_files_to_pdf(file_paths, output_path)
+   file_paths = filedialog.askopenfilenames(title="Select Files", filetypes=[("Text files, Pdf files, Image files", "*.txt *.pdf *.jpg *.jpeg *.png")])
+   if file_paths == None or file_paths == []:
+       raise ImportError("Files were not imported, try again!")
+
+   if action == "convert":
+       for file_path in file_paths:
+           selected_font = font_combobox.get()
+           selected_size = int(text_size_combobox.get())
+           output_path = filedialog.askdirectory(title="Select Output Directory")
+           convert_txtfile_to_pdf(file_path, output_path, selected_font, selected_size)
+   elif action == "merge":
+       if len(file_paths) <= 1:
+           raise ValueError("Can't merge a single file, select more than one to continue!")
+       output_path = filedialog.askdirectory(title="Select Output Directory")
+       merging_pdf(file_paths, output_path)
+   elif action == "convert_img_to_pdf":
+       output_path = filedialog.askdirectory(title="Select Output Directory")
+       convert_img_files_to_pdf(file_paths, output_path)
 
 
 def center_window(window, width, height):
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
+   screen_width = window.winfo_screenwidth()
+   screen_height = window.winfo_screenheight()
 
-    x_coordinate = (screen_width / 2) - (width / 2)
-    y_coordinate = (screen_height / 2) - (height / 2)
+   x_coordinate = (screen_width / 2) - (width / 2)
+   y_coordinate = (screen_height / 2) - (height / 2)
 
-    window.geometry(f"{width}x{height}+{int(x_coordinate)}+{int(y_coordinate)}")
+   window.geometry(f"{width}x{height}+{int(x_coordinate)}+{int(y_coordinate)}")
 
 
 def main():
@@ -210,6 +214,11 @@ def main():
     font_combobox.current(0)
     font_combobox.pack(pady=5)
 
+    global text_size_combobox
+    text_size_combobox = ttk.Combobox(convert_frame, values=text_size, state="readonly")
+    text_size_combobox.current(7)
+    text_size_combobox.pack(pady=5)
+
     merge_frame = tk.Frame(notebook)
     notebook.add(merge_frame, text='Merge PDFs')
 
@@ -238,7 +247,6 @@ def main():
 
     center_window(root, 600, 600)
     root.mainloop()
-
 
 if __name__ == "__main__":
     main()
